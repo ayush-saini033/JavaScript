0601143b75731378d9476c8301c4e15bd2976d1a
Ayush <ayushsaini1910@gmail.com>
2024-08-12 22:26:50 +0530

    second

---
+++ BasicJS/E_Conversion.js
@@ -0,0 +1,21 @@
+let score = 33
+let num = "33"
+console.log(typeof score)
+console.log(typeof num)
+console.log(typeof Number(num))
+
+let n = "33ff"
+let m = Number("ad34")
+console.log(typeof Number(n))
+console.log(typeof Number(m))
+console.log(typeof Number("Ayush"))
+console.log(m)
+
+let str = "Ayush"
+let k = Number(str)
+
+console.log(k)
+
+let next = "ff34"
+let s = Number(next)
+console.log(s)
