b0023528fc6b0fc205cc1c249449bd4d6b348080
Ayush <ayushsaini1910@gmail.com>
2024-08-12 22:42:59 +0530

    compare

---
+++ BasicJS/C_Comparison.js
@@ -0,0 +1,10 @@
+console.log(null > 0) // false 
+console.log(null == 0) // false
+console.log(null >= 0) // true
+
+// undefined comparison always false
+
+// Strict change === , it check the data types also
+
+console.log("12" == 12) // true
+console.log("12" === 12) // false
