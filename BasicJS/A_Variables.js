e906c7286034f2b77c768508268c8377aeb4cf1e
Ayush <ayushsaini1910@gmail.com>
2024-08-12 21:36:32 +0530

    1st

---
+++ BasicJS/A_Variables.js
@@ -0,0 +1,14 @@
+const roll = 33
+let email = 'ayushsaini1910@gamil.com'
+var city = 'Alwar'
+
+console.table([roll,email,city])
+
+// scope
+
+{
+    city = 'Jaipur'
+    let = '23bce033@nith.ac.in'
+}
+
+console.table([roll,email,city])
---
+++ BasicJS/B_DataTypes.js
@@ -0,0 +1,12 @@
+"use strict"; // treat all JS code as newer version
+
+// alert(3 + 3)  we are using nodejs, not browser
+
+let name = "hitesh"
+let age = 18
+let flag = true
+// null => standlone value
+// symbol => uniqe
+console.log(typeof age)
+console.log(typeof undefined)
+console.log(typeof null)
