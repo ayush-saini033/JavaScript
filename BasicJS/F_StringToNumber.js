b20b05eb8f2fdc607042c4ec52dfa57c2c2fd3c2
Ayush <ayushsaini1910@gmail.com>
2024-08-12 22:35:59 +0530

    stringConv1

---
+++ BasicJS/F_StringToNumber.js
@@ -0,0 +1,26 @@
+let value = 3
+let negValue = -value
+
+console.log(negValue)
+
+let str1 = "Ayush"
+let str2 = " Saini"
+
+console.log(str1 + str2)
+
+console.log(1 + 3 + "4" + 5 + 7)
+console.log(+true)
+console.log(+"")
+
+let num1, num2, num3
+num1 = num2 = num3 = 4
+
+let counter = 100
+
+counter++
+console.log(counter)
+console.log(counter++)
+console.log(counter)
+
+++counter
+console.log(counter)
