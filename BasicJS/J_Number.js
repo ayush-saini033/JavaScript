43ad28374ce738d4df761a1d83ae1c20836e1ef3
Ayush <ayushsaini1910@gmail.com>
2024-08-13 17:41:13 +0530

    secondMath

--- BasicJS/J_Number.js
+++ BasicJS/J_Number.js
@@ -34,4 +34,4 @@ console.log(Math.floor(Math.random()*10) + 1)
 const min = 10
 const max = 20
 
-console.log(Math.floor(Math.random() * (max - min + 1)) + min)n
+console.log(Math.floor(Math.random() * (max - min + 1)) + min)
